aaff539f13dbe6ec6c23eefa57e01b8f41d7dcf1
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2023-12-08 17:51:27 -0700

    style: organized import statments

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .models import User
-from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
